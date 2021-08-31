48746d1599d29bc5152eb1836c95ce8bd94fc142
BohdanKarp <81150034+BohdanKarp@users.noreply.github.com>
2021-08-31 12:00:14 +0300

    add basic return-negative

---
+++ Introduction/Tasks/Basic/9/return-negative.java
@@ -0,0 +1,15 @@
+public class Kata {
+
+    public static int makeNegative(final int x) {
+        int z;
+        if (x > 0) {
+            z = -x;
+        } else {
+            z = x;
+        }
+
+        return z; // Your code here.
+
+    }
+
+}

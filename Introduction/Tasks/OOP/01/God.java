38ff2ec892fa6f08ae512a9e1fa408385724da00
BohdanKarp <81150034+BohdanKarp@users.noreply.github.com>
2021-09-06 11:55:52 +0300

    add God

---
+++ Introduction/Tasks/OOP/01/God.java
@@ -0,0 +1,18 @@
+public class God {
+    public static Human[] create(){
+        Human[] humans =new Human[2];
+        humans[0]=new Man();
+        humans[1]=new Woman();
+        return humans;
+    }
+}
+
+abstract class Human {
+
+}
+class Man extends Human{
+
+}
+class Woman extends Human{
+
+}
